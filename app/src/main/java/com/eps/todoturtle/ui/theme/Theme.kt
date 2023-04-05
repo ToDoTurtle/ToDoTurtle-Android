0bd7268d51805bde4ec2cde88d0274f1a1f2b1bf
FerranAD <ferranaran@tuta.io>
2023-04-05 13:26:07 +0200

    feat(colorscheme): add custom color support

--- app/src/main/java/com/eps/todoturtle/ui/theme/Theme.kt
+++ app/src/main/java/com/eps/todoturtle/ui/theme/Theme.kt
@@ -1,12 +1,14 @@
 package com.eps.todoturtle.ui.theme
 
+
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -37,6 +39,7 @@ private val LightColors = lightColorScheme(
     inversePrimary = md_theme_light_inversePrimary,
     surfaceTint = md_theme_light_surfaceTint,
 )
+
 
 private val DarkColors = darkColorScheme(
     primary = md_theme_dark_primary,
@@ -79,7 +82,7 @@ val ColorScheme.inactiveOnPrimaryContainer: Color
 @Composable
 fun ToDoTurtleTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit,
+    content: @Composable() () -> Unit
 ) {
     val colors = if (!useDarkTheme) {
         LightColors
@@ -89,6 +92,6 @@ fun ToDoTurtleTheme(
 
     MaterialTheme(
         colorScheme = colors,
-        content = content,
+        content = content
     )
 }
