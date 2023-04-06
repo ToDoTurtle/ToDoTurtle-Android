4ac204e3681c940290fd40c9cf2c89f455baa599
Oriol Agost Batalla <gpg18@agost.info>
2023-04-06 12:58:28 +0200

    feat: camera picture implementation

--- app/src/main/java/com/eps/todoturtle/ui/profile/picture/DialogPicture.kt
+++ app/src/main/java/com/eps/todoturtle/ui/profile/picture/DialogPicture.kt
@@ -1,23 +1,25 @@
 package com.eps.todoturtle.ui.profile.picture
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.eps.todoturtle.R
 
 @Composable
-fun Picture(
-    profilePicture: Int,
+fun DialogPicture(
+    bitmap: Bitmap,
 ) {
     Box(
         modifier = Modifier
@@ -33,7 +35,8 @@ fun Picture(
             ),
         ) {
             Image(
-                painter = painterResource(id = profilePicture),
+                bitmap = bitmap.asImageBitmap(),
+                modifier = Modifier.size(400.dp),
                 contentDescription = stringResource(id = R.string.profile_picture_desc),
             )
         }
