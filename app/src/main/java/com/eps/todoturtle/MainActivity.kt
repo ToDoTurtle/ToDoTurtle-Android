4c5ed5702221802005a43f6f694c9dbf435dcea7
Oriol Agost Batalla <gpg18@agost.info>
2023-04-11 15:52:24 +0200

    feat: add permissions request

--- app/src/main/java/com/eps/todoturtle/MainActivity.kt
+++ app/src/main/java/com/eps/todoturtle/MainActivity.kt
@@ -3,13 +3,33 @@ package com.eps.todoturtle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.eps.todoturtle.permissions.PermissionRequester
+import com.eps.todoturtle.permissions.RequestPermissionContext
+import com.eps.todoturtle.permissions.providers.CameraPermissionProvider
+import com.eps.todoturtle.permissions.providers.ReadExternalStoragePermissionProvider
 import com.eps.todoturtle.ui.MainActivityUI
+import com.eps.todoturtle.ui.theme.ToDoTurtleTheme
 
 class MainActivity : ComponentActivity() {
+    private val permissionsToRequest = listOf(
+        CameraPermissionProvider(),
+        ReadExternalStoragePermissionProvider(),
+    )
+
+    private lateinit var permissionRequester: PermissionRequester
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionRequester = PermissionRequester(this, permissionsToRequest)
+
         setContent {
-            MainActivityUI()
+            ToDoTurtleTheme {
+                RequestPermissionContext(permissionRequester) {
+                    requestPermissions()
+                    MainActivityUI()
+                }
+            }
         }
     }
 }
