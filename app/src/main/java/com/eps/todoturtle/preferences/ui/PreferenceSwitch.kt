8956072011c3c9dea572d952822144ce85efeede
Oriol Agost Batalla <gpg18@agost.info>
2023-04-15 00:55:45 +0200

    refactor: tweak switch preference colors

--- app/src/main/java/com/eps/todoturtle/preferences/ui/PreferenceSwitch.kt
+++ app/src/main/java/com/eps/todoturtle/preferences/ui/PreferenceSwitch.kt
@@ -15,10 +15,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -64,6 +66,14 @@ internal fun PreferenceSwitch(
                 Spacer(modifier = Modifier.weight(1f))
                 Switch(
                     checked = checked,
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.White,
+                        checkedTrackColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                        checkedBorderColor = MaterialTheme.colorScheme.tertiary,
+                        uncheckedThumbColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                        uncheckedTrackColor = Color.White,
+                        uncheckedBorderColor = MaterialTheme.colorScheme.tertiary,
+                    ),
                     onCheckedChange = {
                         onCheckedChange()
                     }
