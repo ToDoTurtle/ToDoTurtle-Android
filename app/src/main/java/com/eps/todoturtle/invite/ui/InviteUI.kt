92e9f4d6a5737d66f32de65a8d0de68ced95e7e7
Oriol Agost Batalla <gpg18@agost.info>
2023-04-17 10:34:48 +0200

    feat: implements invite under construction screen

--- app/src/main/java/com/eps/todoturtle/invite/ui/InviteUI.kt
+++ app/src/main/java/com/eps/todoturtle/invite/ui/InviteUI.kt
@@ -1,27 +1,29 @@
 package com.eps.todoturtle.invite.ui
 
+import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Size
 import com.eps.todoturtle.R
 import com.eps.todoturtle.shared.logic.extensions.RobotoThin
 
@@ -35,39 +37,38 @@ fun InviteUI() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Icon(
+            val context = LocalContext.current
+            val imageLoader = ImageLoader.Builder(context)
+                .components {
+                    if (SDK_INT >= 28) {
+                        add(ImageDecoderDecoder.Factory())
+                    } else {
+                        add(GifDecoder.Factory())
+                    }
+                }
+                .build()
+            Image(
                 modifier = Modifier
                     .fillMaxHeight(0.5f)
                     .fillMaxWidth(0.5f),
-//                    .offset(y = (-100).dp),
-                painter =  painterResource(id = R.drawable.construction),
-                contentDescription = "Prova",
-                tint = Color.LightGray,
+                painter = rememberAsyncImagePainter(
+                    ImageRequest.Builder(context)
+                        .data(data = R.drawable.drill).apply(block = {
+                            size(Size.ORIGINAL)
+                        }).build(),
+                    imageLoader = imageLoader,
+                ),
+                contentDescription = null,
             )
-            Column(
-                verticalArrangement = Arrangement.Top
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    text = "Under construction",
-                    style = MaterialTheme.typography.titleLarge,
-                    textAlign = TextAlign.Center,
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily.RobotoThin,
-                    color = Color.LightGray,
-                )
-                Text(
-                    modifier = Modifier
-                        .padding(start = 10.dp, end = 10.dp)
-                        .fillMaxWidth(),
-                    text = "This section is still in development, wait a bit while we finish implementing it.",
-                    style = MaterialTheme.typography.headlineSmall,
-//                    textAlign = TextAlign.Center,
-                    fontFamily = FontFamily.RobotoThin,
-                    color = Color.LightGray,
-                )
-            }
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Under construction ...",
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontFamily = FontFamily.RobotoThin,
+            )
         }
     }
 }
