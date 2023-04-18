3a2e2d4068212c4d810dab55278550fb78380740
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-04-18 10:31:08 +0200

    feat: improve write to device activity feedback

--- app/src/main/java/com/eps/todoturtle/nfc/WriteToDeviceActivity.kt
+++ app/src/main/java/com/eps/todoturtle/nfc/WriteToDeviceActivity.kt
@@ -1,6 +1,8 @@
 package com.eps.todoturtle.nfc
 
+import android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.eps.todoturtle.R
 import com.eps.todoturtle.nfc.logic.NfcWriteViewModel.INIT.getNfcWriteModel
 import com.eps.todoturtle.nfc.ui.WriteDevice
 import com.eps.todoturtle.shared.logic.extensions.dataStore
@@ -23,9 +26,29 @@ class WriteToDeviceActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    WriteDevice(viewModel = viewModel)
+                    WriteDevice(viewModel = viewModel,
+                        onTagNotWriteable = ::restartActivity,
+                        onTagLost = ::restartActivity,
+                        unknownError = ::restartActivity,
+                        onNfcNotSupported = {
+                            Toast.makeText(this, getString(R.string.nfc_not_supported_solution), Toast.LENGTH_SHORT).show()
+                            finish()
+                        },
+                        onWriteSuccessful = {
+                            Toast.makeText(this, getString(R.string.nfc_write_success), Toast.LENGTH_SHORT)
+                                .show()
+                            finish()
+                        })
                 }
             }
         }
     }
+
+    private fun restartActivity() {
+        finish()
+        startActivity(intent.apply {
+            addFlags(FLAG_ACTIVITY_NO_ANIMATION)
+        })
+    }
 }
+
