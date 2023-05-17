dc743710758b3d460f42cc65977a9d8fa564b047
Oriol Agost Batalla <gpg18@agost.info>
2023-05-17 18:49:23 +0200

    feat: adds cangrejos app

--- app/src/main/java/com/eps/todoturtle/aboutus/logic/routes/AboutUsRoutes.kt
+++ app/src/main/java/com/eps/todoturtle/aboutus/logic/routes/AboutUsRoutes.kt
@@ -1,7 +1,7 @@
 package com.eps.todoturtle.aboutus.logic.routes
 
 enum class AboutUsRoutes(private val value: String) {
-    IMAGE("https://gist.githubusercontent.com/oriolagobat/a688827df49e39ad8f107d6c25325407/raw/5252a5cae95fb6941ec5569deb46d32503174d52/about_us_image.txt"),
+    IMAGE("https://gist.github.com/oriolagobat/a688827df49e39ad8f107d6c25325407/raw/ee80f49ae15f89491f99fd249178f09f6ee14f7f/about_us_image.txt"),
     TEXT("https://gist.githubusercontent.com/oriolagobat/786ac67637baa60ce4e4ad79c88de533/raw/709233942753e393eb768bf7e2d398055b60537d/about_us.txt"),
     ;
 
