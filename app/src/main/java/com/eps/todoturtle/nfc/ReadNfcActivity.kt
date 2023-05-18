d0ac10a3c794fed78b2e2e67f3ce21ac2c9b3d8d
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-18 12:35:34 +0200

    feat: read nfc from device

--- app/src/main/java/com/eps/todoturtle/nfc/ReadNfcActivity.kt
+++ app/src/main/java/com/eps/todoturtle/nfc/ReadNfcActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -11,40 +12,62 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.eps.todoturtle.action.infra.FirebaseActionRepository
+import com.eps.todoturtle.action.logic.ActionViewModel.Companion.getActionViewModel
+import com.eps.todoturtle.action.logic.NoteAction
 import com.eps.todoturtle.nfc.ui.AnimatedDisappearingText
 import com.eps.todoturtle.shared.logic.extensions.dataStore
 import com.eps.todoturtle.ui.theme.ToDoTurtleTheme
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ReadNfcActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val message = getMessage()
-        // Here we should handle the message (uuid) properly, for now, we're printing it
+        val id = getId()
+        if (Firebase.auth.currentUser == null) {
+            Toast.makeText(this, "Please login to continue", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        if (id == null) {
+            Toast.makeText(this, "Error reading id!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        val actionRepository = FirebaseActionRepository()
+        val viewModel = getActionViewModel(actionRepository)
+        val action = viewModel.actions.get(id)
+        if (action == null) {
+            Toast.makeText(this, "Device doesn't have any action, please configure it for use them", Toast.LENGTH_SHORT).show()
+            finish()
+        }
         setContent {
             ToDoTurtleTheme(dataStore) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    ShowNfcMessage(message)
+                    action?.let {
+                        ShowAction(action)
+                    }
                 }
             }
         }
     }
 
-    private fun getMessage(): String {
+    private fun getId(): String? {
         if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
-            return getMessage(intent)
+            return getId(intent)
         }
-        return "Error getting NFC"
+        return null
     }
 
-    private fun getMessage(intent: Intent): String {
+    private fun getId(intent: Intent): String {
         val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
         with(parcelables) {
             val inNdefMessage = this?.get(0) as NdefMessage
@@ -55,13 +78,18 @@ class ReadNfcActivity : ComponentActivity() {
 }
 
 @Composable
-fun ShowNfcMessage(name: String, modifier: Modifier = Modifier) {
+fun ShowAction(action: NoteAction, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize(),
     ) {
-        AnimatedDisappearingText(text = "Detected device with id: $name")
+        Text(text = "Note Action Info:")
+        Text(text = "Title: ${action.title}")
+        Text(text = "Description: ${action.description}")
+        Text(text = "Get Location: ${action.getLocation}")
+        Text(text = "Deadline: ${action.deadline}")
+        Text(text = "Notification: ${action.notification}")
     }
 }
 
@@ -69,6 +97,12 @@ fun ShowNfcMessage(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ToDoTurtleTheme(LocalContext.current.dataStore) {
-        ShowNfcMessage("FakeId1234")
+        ShowAction(NoteAction(
+            title = "Dummy Title",
+            description = "Dummy Description",
+            getLocation = false,
+            deadline = null,
+            notification = null,
+        ))
     }
 }
