9e52c05da4a8f0eecb57b5578fffd6970b1e18bf
Oriol Agost Batalla <gpg18@agost.info>
2023-05-19 17:38:12 +0200

    feat: implements default/cutom image through firebase storage

--- app/src/main/java/com/eps/todoturtle/profile/logic/DetailsPictureStorage.kt
+++ app/src/main/java/com/eps/todoturtle/profile/logic/DetailsPictureStorage.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import com.eps.todoturtle.shared.logic.extensions.bitmapFrom
 import com.eps.todoturtle.shared.logic.extensions.byteArrayFrom
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageException
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.tasks.await
 
@@ -15,13 +16,20 @@ class DetailsPictureStorage(
     private val defaultPictureRef = storage.reference.child("default_pfp.jpg")
 
     suspend fun getProfileImage(): Bitmap {
-        return bitmapFrom(
-            try {
-                customPictureRef.getBytes(Long.MAX_VALUE).result
-            } catch (ignored: Throwable) {
-                defaultPictureRef.getBytes(Long.MAX_VALUE).await()
-            },
-        )
+        return if (customExists()) {
+            bitmapFrom(customPictureRef.getBytes(1024 * 1024).await())
+        } else {
+            bitmapFrom(defaultPictureRef.getBytes(1024 * 1024).await())
+        }
+    }
+
+    private suspend fun customExists(): Boolean {
+        return try {
+            customPictureRef.metadata.await()
+            true
+        } catch (exception: StorageException) {
+            false
+        }
     }
 
     suspend fun setProfileImage(bitmap: Bitmap) {
