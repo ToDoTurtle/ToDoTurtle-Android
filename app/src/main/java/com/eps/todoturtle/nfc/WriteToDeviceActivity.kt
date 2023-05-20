f8bc941a0f1d35a22af294cbedeb4a02597ea4ad
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-20 18:15:20 +0200

    feat: write to device activity

--- app/src/main/java/com/eps/todoturtle/nfc/WriteToDeviceActivity.kt
+++ app/src/main/java/com/eps/todoturtle/nfc/WriteToDeviceActivity.kt
@@ -1,6 +1,6 @@
 package com.eps.todoturtle.nfc
 
-import android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
@@ -8,77 +8,144 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.eps.todoturtle.IconApp
+import com.eps.todoturtle.R
+import com.eps.todoturtle.action.infra.FirebaseActionRepository
+import com.eps.todoturtle.devices.infra.FirebaseDeviceRepository
+import com.eps.todoturtle.devices.logic.DeviceConfigurationParams
+import com.eps.todoturtle.devices.logic.DeviceIconActivity
+import com.eps.todoturtle.devices.logic.DevicesViewModel.Companion.getDevicesViewModel
+import com.eps.todoturtle.devices.ui.DeviceConfigurationScreen
+import com.eps.todoturtle.nfc.logic.NfcWriteViewModel.INIT.getNfcWriteModel
+import com.eps.todoturtle.nfc.ui.WriteDevice
 import com.eps.todoturtle.shared.logic.extensions.dataStore
 import com.eps.todoturtle.ui.theme.ToDoTurtleTheme
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.maltaisn.icondialog.IconDialog
+import com.maltaisn.icondialog.IconDialogSettings
 import com.maltaisn.icondialog.data.Icon
+import com.maltaisn.icondialog.pack.IconDrawableLoader
 import com.maltaisn.icondialog.pack.IconPack
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
 
-class WriteToDeviceActivity : AppCompatActivity(), IconDialog.Callback {
+class WriteToDeviceActivity : AppCompatActivity(), IconDialog.Callback, DeviceIconActivity {
 
     private val callBackIcons: MutableStateFlow<Int?> = MutableStateFlow(null)
+    private val currentIcon: Channel<Int> = Channel(UNLIMITED)
+    private lateinit var iconDialog: IconDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val iconDialog = supportFragmentManager.findFragmentByTag(ICON_DIALOG_TAG) as IconDialog?
-//            ?: IconDialog.newInstance(IconDialogSettings())
-//        val viewModel = getNfcWriteModel()
+
+        iconDialog = supportFragmentManager.findFragmentByTag(ICON_DIALOG_TAG) as IconDialog?
+            ?: IconDialog.newInstance(IconDialogSettings())
+        theme.applyStyle(R.style.AppTheme, true)
+
+        if (Firebase.auth.currentUser == null) {
+            Toast.makeText(this, "Please login to add a device", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        val nfcWriteViewModel = getNfcWriteModel()
+        val devicesViewModel = getDevicesViewModel(
+            repository = FirebaseDeviceRepository(),
+            actionsRepository = FirebaseActionRepository()
+        )
         setContent {
             ToDoTurtleTheme(LocalContext.current.dataStore) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-//                    WriteDevice(
-//                        viewModel = viewModel,
-//                        onTagNotWriteable = ::restartActivity,
-//                        onTagLost = ::restartActivity,
-//                        unknownError = ::restartActivity,
-//                        onNfcNotSupported = {
-//                            Toast.makeText(
-//                                this,
-//                                getString(R.string.nfc_not_supported_solution),
-//                                Toast.LENGTH_SHORT,
-//                            ).show()
-//                            finish()
-//                        },
-//                        onWriteSuccessful = {
-//                            Toast.makeText(
-//                                this,
-//                                getString(R.string.nfc_write_success),
-//                                Toast.LENGTH_SHORT,
-//                            )
-//                                .show()
-//                            finish()
-//                        },
-//                    )
+                    var showDevices by rememberSaveable { mutableStateOf(false) }
+                    if (!showDevices) {
+                        WriteDevice(
+                            viewModel = nfcWriteViewModel,
+                            onNfcNotSupported = {
+                                Toast.makeText(this, "NFC not supported", Toast.LENGTH_SHORT).show()
+                                finish()
+                            },
+                            onTagLost = {
+                                Toast.makeText(
+                                    this,
+                                    "TAG Lost, please try again",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                                finish()
+                            },
+                            onTagNotWriteable = {
+                                Toast.makeText(
+                                    this,
+                                    "TAG can't be written, buy another one",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                finish()
+                            },
+                            unknownError = {
+                                Toast.makeText(
+                                    this,
+                                    "Unknown error, please try again",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                finish()
+                            },
+                            onWriteSuccessful = { id ->
+                                nfcWriteViewModel.finishWriteNfc()
+                                devicesViewModel.deviceBuilder.identifier = id
+                                showDevices = true
+                            },
+                        )
+                    } else {
+                        DeviceConfigurationScreen(devicesViewModel, DeviceConfigurationParams.NEW) {
+                            Toast.makeText(
+                                this,
+                                "Device ${it.name} added successfully, enter the app for customizing it",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            finish()
+                        }
+                    }
                 }
             }
         }
-    }
-
-    private fun restartActivity() {
-        finish()
-        startActivity(
-            intent.apply {
-                addFlags(FLAG_ACTIVITY_NO_ANIMATION)
-            },
-        )
     }
 
     override val iconDialogIconPack: IconPack?
         get() = (application as IconApp).iconPack
 
     override fun onIconDialogIconsSelected(dialog: IconDialog, icons: List<Icon>) {
-        this.callBackIcons.value = icons.last().id
-        Toast.makeText(this, "Icon selected ${icons.last().pathData}", Toast.LENGTH_SHORT).show()
+        runBlocking(Dispatchers.IO) {
+            val icon = icons.last().id
+            currentIcon.send(icon)
+        }
     }
 
     companion object {
         private const val ICON_DIALOG_TAG = "icon-dialog"
     }
+
+    override fun startIconSelectionLambda(): () -> Unit = {
+        iconDialog.show(supportFragmentManager, ICON_DIALOG_TAG)
+    }
+
+    override fun getIconChannel(): Channel<Int> = currentIcon
+
+    override fun getIconDrawable(): (Int) -> Drawable? = ::getIconD
+
+    private fun getIconD(id: Int): Drawable? {
+        val iconDrawableLoader = IconDrawableLoader(this)
+        return iconDialogIconPack?.getIconDrawable(id, iconDrawableLoader)
+    }
+
 }
