4499cfba2c9693d9df9ec0e97f6f19d68e53a68b
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-21 19:13:56 +0200

    fix: firebase push notifications not authentified

--- app/src/main/java/com/eps/todoturtle/notifications/AppFirebaseMessagingService.kt
+++ app/src/main/java/com/eps/todoturtle/notifications/AppFirebaseMessagingService.kt
@@ -1,6 +1,8 @@
 package com.eps.todoturtle.notifications
 
 import com.eps.todoturtle.shared.infra.addDeviceToken
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
@@ -9,6 +11,7 @@ class AppFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        if (Firebase.auth.currentUser == null) return
         runBlocking(Dispatchers.IO) {
             addDeviceToken(token)
         }
